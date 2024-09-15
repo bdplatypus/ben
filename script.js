2786f16f2abf73a07325d6df25b39fc77d07aca5
bdplatypus <ben.plowman@cantab.net>
2024-09-15 09:54:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function update() {
-  document.getElementById("div").innerHTML += "HI!!! The javascript is working!"
+  document.getElementById("div").innerHTML += "<h1>HI!!! The javascript is working!</h1>"
 }
 update();
